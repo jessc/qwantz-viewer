e4da6b502641a754be0ae8cd78c6a9d337943e83
Jesse Cummins <me@jessecummins.com>
2013-07-06 21:05:25 -0400

    Add initial qwantz_viewer.rb Shoes app

---
+++ qwantz_viewer.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# 2013-07
+# Jesse Cummins
+# https://github.com/jessc
+
+
+=begin
+# bug list:
+# todo:
+
+=end
+
+
+Shoes.app :title => "Qwantz Viewer", :width => 640, :height => 480 do
+
+end
