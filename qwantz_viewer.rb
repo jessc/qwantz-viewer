dad93b1977b2f85849cd7d3970525e4a8c530323
Jesse Cummins <me@jessecummins.com>
2013-07-07 11:42:59 -0400

    Add "Dinosaur Comics" title; add placeholder Previous and Next buttons

--- qwantz_viewer.rb
+++ qwantz_viewer.rb
@@ -12,12 +12,20 @@
 =end
 
 
-Shoes.app :title => "Qwantz Viewer", :width => 900, :height => 675 do
+Shoes.app :title => "Qwantz Viewer", :width => 852, :height => 639 do
 
   stack :margin => 20 do
-    comic = image "http://www.qwantz.com/comics/comic2-14.png"
+    flow do
+      para "Dinosaur Comics"
+      comic = image "http://www.qwantz.com/comics/comic2-14.png"
+    end
 
     flow :margin => 20 do
+      button "Previous" do
+      end
+      button "Next" do
+      end
+
       para "Comic Number:"
       comic_number_line = edit_line
       comic_number_line.text = "14"
